9a42cc9b536107ef69882effe1098752f3060141
rej <rzioma@acm.org>
2024-04-01 14:59:16 +0200

    Renamed 1_58bit to i4_mul_i8

--- test/tb.v
+++ test/tb.v
@@ -26,7 +26,7 @@ module tb ();
     parameter integer COMPUTE_SLICES = `COMPUTE_SLICES;
 
     // Replace tt_um_example with your module name:
-    tt_um_rejunity_1_58bit user_project (
+    tt_um_rejunity_i4_mul_i8 user_project (
         // Include power ports for the Gate Level test:
         `ifdef GL_TEST
             .VPWR(1'b1),
